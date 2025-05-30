5eeba89e01655b8a26651607b1a4a6e5367aeeeb
kosuke978 <heitiankangjie487@gmail.com>
2025-05-31 01:32:41 +0900

    修正失敗

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 from fastapi import FastAPI, Request
 from linebot import LineBotApi, WebhookParser,WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import MessageEvent,  TextSendMessage
 import os
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
 from starlette.exceptions import HTTPException
 
-app = FastAPI()
 
-@app.get("/", response_class=PlainTextResponse)
-async def root():
-    return "LINE BOT is running"
 
 
 load_dotenv()
@@ -20,6 +16,9 @@ load_dotenv()
 LINE_BOT_API=LineBotApi(os.environ["CHANNEL_ACCESS_TOKEN"])
 handler=WebhookHandler(os.environ["CHANNEL_SECRET"])
 app = FastAPI()
+@app.get("/", response_class=PlainTextResponse)
+async def root():
+    return "LINE BOT is running"
 @app.get("/")
 def root():
     return {"message": "LINE BOT is running"}
@@ -29,7 +28,7 @@ CHANNEL_ACCESS_TOKEN = os.getenv("CHANNEL_ACCESS_TOKEN")
 CHANNEL_SECRET = os.getenv("CHANNEL_SECRET")
 
 print(CHANNEL_ACCESS_TOKEN)
-parser = WebhookParser(CHANNEL_SECRET)
+handler = WebhookHandler(CHANNEL_SECRET)
 
 
 @app.post("/webhook")
@@ -38,14 +37,14 @@ async def callback(request: Request):
     signature = request.headers.get("X-Line-Signature", "")
 
     try:
-        events = parser.parse(body.decode("utf-8"), signature)
+        events = handler.parse(body.decode("utf-8"), signature)
     except InvalidSignatureError:
         raise HTTPException(status_code=400, detail="Invalid signature")
 
     for event in events:
-        if isinstance(event, MessageEvent) and isinstance(event.message, TextMessage):
+        if isinstance(event, MessageEvent) and isinstance(event.message, TextSendMessage):
             reply_text = f"あなたのメッセージ: {event.message.text}"
-            line_bot_api.reply_message(
+            LINE_BOT_API.reply_message(
                 event.reply_token,
                 TextSendMessage(text=reply_text)
             )
@@ -58,5 +57,5 @@ def handle_message(event):
      message_text = event.message.text.lower()
      
      if "こんにちは" in message_text:
-        message = TextMessage(text="こんにちは！！")
+        message = TextSendMessage(text="こんにちは！！")
         LINE_BOT_API.reply_message(event.reply_token, message)
